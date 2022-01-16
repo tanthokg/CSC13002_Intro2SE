46295e94c4c685006ef1dba6274d8849f8b0e4d1
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2022-01-16 22:39:42 +0700

    Add cancel button

--- Sunshine/app/src/main/java/com/example/sunshine/user/SettingEditProfileActivity.java
+++ Sunshine/app/src/main/java/com/example/sunshine/user/SettingEditProfileActivity.java
@@ -3,6 +3,7 @@ package com.example.sunshine.user;
 import static android.content.ContentValues.TAG;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -11,8 +12,10 @@ import android.text.InputType;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -39,7 +42,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class SettingEditProfileActivity extends AppCompatActivity {
 
@@ -50,12 +56,14 @@ public class SettingEditProfileActivity extends AppCompatActivity {
     ImageView imgAvt;
     //TextView usernameTv,fullNameTv,birthdayTv,genderTv;
     EditText usernameET,fullNameET,birthdayET;
+    //TextView birthdayTv;
    Spinner gender_options;
     Context context;
     String _username, _fullName, _birthday;
-    MaterialButton confirmBtn;
+    MaterialButton confirmBtn, cancelBtn;
 
     private boolean _gender;
+    final Calendar myCalendar= Calendar.getInstance();
 
 
     @Override
@@ -72,32 +80,19 @@ public class SettingEditProfileActivity extends AppCompatActivity {
         fullNameET = (EditText) findViewById(R.id.fullnameBox);
       //  genderET = (TextView)findViewById(R.id.genderBox);
         confirmBtn =(MaterialButton) findViewById(R.id.confirmEditProfile);
+        cancelBtn = (MaterialButton) findViewById(R.id.cancelEditProfile);
 
         gender_options = (Spinner) findViewById(R.id.gender_option);
-     adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, gender);
-//      adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, gender);
+
 //        gender_options.setAdapter(adapter);
         birthdayET = (EditText) findViewById(R.id.birthdayBox);
 
+
         database = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
-        //currentUserId = auth.getCurrentUser().getUid();
-        //getUsername();
-        // usernameTv.setText(username);
         String userId = auth.getCurrentUser().getUid();
-//        database.collection("User").document(userId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task <DocumentSnapshot> task) {
-//                if (task.isSuccessful())
-//                    if (task.getResult() != null) {
-//                        User user;
-//                        user = task.getResult().toObject(User.class);
-//                        username = user.getUsername();
-//                        fullname =user.getFullname();
-//                    }
-//            }
-//        });
-        // Gets user document from Firestore as reference
+
         DocumentReference docRef = database.collection("User").document(userId);
 
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -134,7 +129,22 @@ public class SettingEditProfileActivity extends AppCompatActivity {
                 }
             }
         });
-
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+            }
+        };
+        birthdayET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(SettingEditProfileActivity.this,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+      adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
       confirmBtn.setOnClickListener(new View.OnClickListener() {
 
           @Override
@@ -142,6 +152,14 @@ public class SettingEditProfileActivity extends AppCompatActivity {
           public void onClick(View v) {
               _username = usernameET.getText().toString();
               _fullName = fullNameET.getText().toString();
+              _birthday = birthdayET.getText().toString();
+             String selectedGender = gender_options.getSelectedItem().toString();
+            if(selectedGender.equals("Male")) {
+                _gender = true; }
+              else if (selectedGender.equals("Female"))
+              {
+                  _gender = false;
+              }
 
               DocumentReference docRef = database.collection("User").document(userId);
               docRef
@@ -173,16 +191,75 @@ public class SettingEditProfileActivity extends AppCompatActivity {
                               Log.w(TAG, "Error updating document", e);
                           }
                       });
+              docRef
+                      .update("birthday", _birthday)
+                      .addOnSuccessListener(new OnSuccessListener<Void>() {
+                          @Override
+                          public void onSuccess(Void aVoid) {
+                              Log.d(TAG, "Permission successfully!");
+                          }
+                      })
+                      .addOnFailureListener(new OnFailureListener() {
+                          @Override
+                          public void onFailure(@NonNull Exception e) {
+                              Log.w(TAG, "Error updating document", e);
+                          }
+                      });
+              docRef
+                      .update("gender", _gender)
+                      .addOnSuccessListener(new OnSuccessListener<Void>() {
+                          @Override
+                          public void onSuccess(Void aVoid) {
+                              Log.d(TAG, "Permission successfully!");
+                          }
+                      })
+                      .addOnFailureListener(new OnFailureListener() {
+                          @Override
+                          public void onFailure(@NonNull Exception e) {
+                              Log.w(TAG, "Error updating document", e);
+                          }
+                      });
           }
       });
 
+      cancelBtn.setOnClickListener(new View.OnClickListener() {
 
+          @Override
 
+          public void onClick(View v) {
 
+              setCancelBtn();
 
+          }
+      });
     }
 
-    @Override
+    private void setCancelBtn()
+    {
+        AlertDialog.Builder confirmDialog = new AlertDialog.Builder(this, R.style.AlertDialog);
+        confirmDialog.setMessage("Are you sure to cancel edit profile?");
+        confirmDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(SettingEditProfileActivity.this, "Edit Profile has been cancel", Toast.LENGTH_SHORT).show();
+                onBackPressed();
+            }
+        });
+        confirmDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+
+        confirmDialog.create();
+        confirmDialog.show();
+    }
+    private void updateLabel() {
+        String myFormat = "MM/dd/yyyy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        birthdayET.setText(dateFormat.format(myCalendar.getTime()));
+    }
+        @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId())
         {
@@ -195,10 +272,6 @@ public class SettingEditProfileActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    private void getUsername() {
-
-    }
-
 
 }
 
